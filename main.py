36e2189dccebb75fbaf304bcca5acb58e6dbf26b
ALEKSEY <88752711+DrFess@users.noreply.github.com>
2024-02-19 07:12:35 +0800

    fix session

--- main.py
+++ main.py
@@ -14,12 +14,19 @@ patients = get_patients_from_table('C3:C43')
 patients_out_of_stock = get_patients_from_table('J3:J43')
 patients.extend(patients_out_of_stock)
 for history_number in patients:
-    data = add_diaries(session, int(history_number))
-    pk = data.get('pk')
-    data_1 = get_pk(session, pk)
-    pk_1 = data_1.get('researches')[0].get('pk')
-    data_2 = get_last_diaries(session, pk_1)
-    pk_2 = data_2.get('data')[0].get('pk')
-    local_status = data_by_fields(session, pk_2).get('data').get('1922')
-    result = save_results(connect=session, pk=pk, pk_2=pk_1, local_status=local_status, history_number=history_number)
-    time.sleep(2)
+    try:
+        data = add_diaries(session, int(history_number))
+        pk = data.get('pk')
+        data_1 = get_pk(session, pk)
+        pk_1 = data_1.get('researches')[0].get('pk')
+        data_2 = get_last_diaries(session, pk_1)
+        pk_2 = data_2.get('data')[0].get('pk')
+        local_status = data_by_fields(session, pk_2).get('data').get('1922')
+        result = save_results(connect=session, pk=pk, pk_2=pk_1, local_status=local_status, history_number=history_number)
+        print(f'Дневник для истории №{history_number} создан')
+        time.sleep(2)
+    except Exception as e:
+        print(f'Error: {e}')
+        continue
+
+session.close()
